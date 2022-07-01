394e4d52b0873ec179b90171ef140d42ca8bcdc7
NindroidA <88803583+NindroidA@users.noreply.github.com>
2022-06-30 19:22:18 -0500

    Create InitialScript.sh

---
+++ Network Interface Reboot/InitialScript.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+#Make a temporary directory
+sudo mkdir /temp
+
+#Download after-restart bash script and put it in temporary directory
+sudo curl -o /temp/AfterBoot.sh https://raw.githubusercontent.com/NindroidA/Mac-Automation/main/Network%20Interface%20Reboot/afterboot.sh
+
+#Download custom plist and put it in LaunchDaemons directory
+sudo curl -o /Library/LaunchDaemons/com.nindroid.NIR.plist https://raw.githubusercontent.com/NindroidA/Mac-Automation/main/Network%20Interface%20Reboot/reboot.plist
+
+#Give plist and bash files correct permissions
+sudo chown root:wheel /Library/LaunchDaemons/com.nindroid.NIR.plist
+sudo chown root:wheel /temp/AfterBoot.sh
+
+#Get the plist to launch after reboot
+sudo launchctl bootstrap system /Library/LaunchDaemons/com.nindroid.NIR.plist
+
+#Disable the Network Interface
+sudo networksetup -setnetworkserviceenabled Wi-Fi off
+
+#Reboot
+sudo reboot
