c6038f50a5539b2793edeb38bde191a8a2a2bde1
NindroidA <88803583+NindroidA@users.noreply.github.com>
2022-06-30 19:36:45 -0500

    Update InitialScript.sh

--- Network Interface Reboot/InitialScript.sh
+++ Network Interface Reboot/InitialScript.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/bash
 
 #Make a temporary directory
-sudo mkdir /temp
+sudo mkdir /Library/temp
 
 #Download after-restart bash script and put it in temporary directory
-sudo curl -o /temp/AfterBoot.sh https://raw.githubusercontent.com/NindroidA/Mac-Automation/main/Network%20Interface%20Reboot/afterboot.sh
+sudo curl -o /Library/temp/AfterBoot.sh https://raw.githubusercontent.com/NindroidA/Mac-Automation/main/Network%20Interface%20Reboot/afterboot.sh
 
 #Download custom plist and put it in LaunchDaemons directory
 sudo curl -o /Library/LaunchDaemons/com.nindroid.NIR.plist https://raw.githubusercontent.com/NindroidA/Mac-Automation/main/Network%20Interface%20Reboot/reboot.plist
 
 #Give plist and bash files correct permissions
 sudo chown root:wheel /Library/LaunchDaemons/com.nindroid.NIR.plist
-sudo chown root:wheel /temp/AfterBoot.sh
+sudo chown root:wheel /Library/temp/AfterBoot.sh
 
 #Get the plist to launch after reboot
 sudo launchctl bootstrap system /Library/LaunchDaemons/com.nindroid.NIR.plist
