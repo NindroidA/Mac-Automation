c9a0b729d551e0e9d56ccb56aa781f3d598cf21e
NindroidA <88803583+NindroidA@users.noreply.github.com>
2022-06-30 20:09:43 -0500

    Update afterboot.sh

--- Network Interface Reboot/afterboot.sh
+++ Network Interface Reboot/afterboot.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
 #Enable the Wi-Fi Interface
-sudo networksetup -setnetworkserviceenabled Wi-Fi on
+networksetup -setnetworkserviceenabled Wi-Fi on
 
 #Bootout the plist
-sudo launchctl bootout system /Library/LaunchDaemons/com.nindroid.NIR.plist
+launchctl bootout system /Library/LaunchDaemons/com.nindroid.NIR.plist
 
 #Remove the plist
-sudo rm /Library/LaunchDaemons/com.nindroid.NIR.plist
+rm /Library/LaunchDaemons/com.nindroid.NIR.plist
 
 #Remove the temporary folder
-sudo rm -r /Library/temp
+rm -r /Library/temp
